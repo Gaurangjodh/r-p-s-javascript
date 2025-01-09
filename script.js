dfb8405fadb2cee7dbf33feceeb835ba8ba5eb4b
Gaurangjodh <jodhgaurang@gmail.com>
2025-01-09 12:02:49 +0530

    Add the playRound function

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let choice = Math.random()*100;
     if(choice < 35){
-        return 'Rock';
+        return 'rock';
     }
     else if(choice < 75){
-        return 'Paper';
+        return 'paper';
     }
     else{
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
@@ -20,3 +23,52 @@ function getHumanChoice(){
     return choice;
 }
 
+function playRound(hC, cC){
+    if(hC === 'rock'){
+        if(cC === 'paper'){
+            computerScore += 1;
+            return `You Lose! ${cC} beats ${hC}`; 
+        }
+        else if(cC === 'rock'){
+            return 'Its a TIE!';
+        }
+        else{
+            humanScore += 1;
+            return `You WIN! ${hC} beats ${cC}`;
+        }
+    }
+    if(hC === 'paper'){
+        if(cC === 'scissors'){
+            computerScore += 1;
+            return `You Lose! ${cC} beats ${hC}`; 
+        }
+        else if(cC === 'paper'){
+            return 'Its a TIE!';
+        }
+        else{
+            humanScore += 1;
+            return `You WIN! ${hC} beats ${cC}`;
+        }
+    }
+    if(hC === 'scissors'){
+        if(cC === 'rock'){
+            computerScore += 1;
+            return `You Lose! ${cC} beats ${hC}`; 
+        }
+        else if(cC === 'scissors'){
+            return 'Its a TIE!';
+        }
+        else{
+            humanScore += 1;
+            return `You WIN! ${hC} beats ${cC}`;
+        }
+    }
+    
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+console.log(playRound(humanChoice,computerChoice));
+console.log(`Computer Score = ${computerScore}`);
+console.log(`Human Score = ${humanScore}`);
